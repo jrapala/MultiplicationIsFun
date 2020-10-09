1b057a95823a1cf77fe01943b3862371653cbcca
Juliette Rapala <jrapala@gmail.com>
2020-10-09 17:44:49 -0500

    Initial Commit

---
+++ MultiplicationIsFun/MultiplicationIsFunApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationIsFunApp.swift
+//  MultiplicationIsFun
+//
+//  Created by Juliette Rapala on 10/9/20.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationIsFunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
